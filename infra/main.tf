b64ff58114e069feeb0ec6cccc02670e78f2a7e6
Hongbin <jdmjdm1998@163.com>
2026-09-08 11:24:03 +0100

    infra: include admin secret in application secret set

--- infra/main.tf
+++ infra/main.tf
@@ -12,6 +12,7 @@ locals {
     var.database_url_secret_arn,
     var.gemini_api_key_secret_arn,
     var.deepseek_api_key_secret_arn,
+    var.admin_api_key_secret_arn,
   ])
 
   ecr_lifecycle_policy = jsonencode({
